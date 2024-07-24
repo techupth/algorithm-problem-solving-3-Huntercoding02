52bd78fb3dc92438f07f68ce033763bb06d48ff7
Hunter <hunvicter@gmail.com>
2024-07-24 15:21:47 +0700

    resolved

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,17 @@
 function sortCustomerName(customers) {
   // เริ่มเขียนโค้ดตรงนี้จ้า
+  for(let i=0;i<customers.length-1;i++){
+    for(let j=0;j<customers.length-1-i;j++){
+      if(customers[j]>customers[j+1]){
+        let temp = customers[j]
+        customers[j]=customers[j+1]
+        customers[j+1] = temp
+      }
+    }
+  }
+  return customers
 }
 
+console.log(sortCustomerName(["Somchai", "Arthit", "Suchada", "Napasorn", "Pimchanok"]))
 // ตอบคำถามตรงนี้จ้า
+//O(n^2) จะทำงานเพิ่มขึ้นตามจำนวนรอบของ input ที่ใส่เข้ามา
